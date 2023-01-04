313040aab7559d698c5f3db451d7c7fe539f9623
Khedr Ibrahim <108041341+KhedrIbrahim@users.noreply.github.com>
2023-01-04 09:15:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+//Best Meth
+document.onclick = function (even){
+    console.log(`This is ${even.target.nodeName}`)
+}
+
+
+//Sec Meth
+/*
+let div = document.getElementsByTagName("div")[0];
+let span = document.getElementsByTagName("span")[0];
+let p = document.getElementsByTagName("p")[0];
+let article = document.getElementsByTagName("article")[0];
+let section = document.getElementsByTagName("section")[0];
+div.onclick = function (){
+    console.log(`This Is Div`)
+}
+span.onclick = function (){
+    console.log(`This Is Span`)
+}
+p.onclick = function (){
+    console.log(`This Is P`)
+}
+article.onclick = function (){
+    console.log(`This Is Article`)
+}
+section.onclick = function (){
+    console.log(`This Is Section`)
+}
+*/
